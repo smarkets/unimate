5c7f0cc628e30fa84ab5556951407410cb79030c
Paul Oliver <paul.oliver@smarkets.com>
2011-07-15 09:44:09 +0100

    Exec commands as they're received rather than when the socket is closed

--- src/unimate_command.erl
+++ src/unimate_command.erl
@@ -2,65 +2,78 @@
 
 -export([handle/1]).
 
--export([help/1, broadcast/1, serverinfo/1]).
+-export([help/1, broadcast/2, serverinfo/1]).
 
 -define(SEP, <<"\r\n">>).
 
 -define(COMMANDS, [help, broadcast, quit, serverinfo]).
 
-handle(Socket) ->
-  Data = read_all(Socket),
-  Commands = binary:split(Data, ?SEP, [global]),
-  [exec_command(C) || C <- Commands, C =/= <<>>],
-  gen_tcp:close(Socket).
+handle(Sock) ->
+   handle(Sock, <<>>).
 
-read_all(Socket) ->
-  read_all(Socket, <<>>).
-read_all(Socket, Acc) ->
-  case gen_tcp:recv(Socket, 0) of
-    {ok, Packet} ->
-      read_all(Socket, <<Acc/binary,Packet/binary>>);
-    {error, closed} ->
-      Acc
+handle(Sock, Buf) ->
+   case gen_tcp:recv(Sock, 0) of
+      {ok, Packet} ->
+         Buf2 = pop_commands(Sock, <<Buf/binary,Packet/binary>>),
+         handle(Sock, Buf2);
+      {error, closed} ->
+         pop_commands(Sock, Buf)
+   end.
+
+pop_commands(Sock, Buf) ->
+   case binary:split(Buf, ?SEP) of
+     [<<>>,<<>>] ->
+       <<>>;
+     [E] ->
+       E;
+     [C, R] ->
+       exec(Sock, C),
+       pop_commands(Sock, R)
+   end.
+
+exec(Sock, Data) ->
+  case binary:split(Data, <<" ">>) of
+    [C0, R] ->
+      C = command(C0),
+      ?MODULE:C(Sock, R);
+    [C0] ->
+      C = command(C0),
+      ?MODULE:C(Sock)
   end.
 
-exec_command(<<>>) ->
-  ok;
-exec_command(Data) ->
-  [C0|R] = binary:split(Data, <<" ">>),
-  C = existing_command(C0),
-  apply(?MODULE, C, R).
-
-existing_command(C0) when is_binary(C0) ->
+command(C0) when is_binary(C0) ->
   try
     C = list_to_existing_atom(string:to_lower(binary_to_list(C0))),
     true = lists:member(C, ?COMMANDS),
     C
   catch _:_ ->
-      throw({unknown_command, C0})
+      error_logger:info_msg("Unknown command ~s", [C0])
   end.
 
-help(_Socket) ->
-  %% TODO
-  todo.
+help(Sock) ->
+  Help = ["",
+          "commands:",
+          "",
+          "    help",
+          "    broadcast <chan>? <msg>",
+          "    serverinfo",
+          ""],
+  gen_tcp:send(Sock, string:join(Help, "\n")).
 
-broadcast(Data) ->
+broadcast(_Sock, Data) ->
   case binary:split(Data, <<" ">>) of
     [Room, Rest] ->
-      try
-        case unimate_xmpp_client:is_room(Room) of
-          true ->
-            unimate_xmpp_client:send_groupchat(Rest, Room);
-          false ->
-            unimate_xmpp_client:send_groupchat(Data)
-        end
-      catch _:_ ->
+      case unimate_xmpp_client:is_room(Room) of
+        true ->
+          unimate_xmpp_client:send_groupchat(Rest, Room);
+        false ->
           unimate_xmpp_client:send_groupchat(Data)
       end;
     _ ->
       unimate_xmpp_client:send_groupchat(Data)
   end.
 
-serverinfo(_Socket) ->
-  %% TODO
-  todo.
+serverinfo(Sock) ->
+  I = io_lib:format("~p\n~p\n",
+                    [erlang:memory(), application:loaded_applications()]),
+  gen_tcp:send(Sock, I).
